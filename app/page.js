4c41120e2354c31b764bc6bab7a54b68c1133f4b
Abdelrahman <124844787+abdelrahman-mohamed8568@users.noreply.github.com>
2025-08-13 12:38:18 +0300

    Add ReactLenis for smooth scrolling; update package dependencies

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import Navbar from "./components/navbar";
 import Btn from "./components/btn";
+import ReactLenis from "lenis/react";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         href="https://cdn.jsdelivr.net/npm/nimbus-sans-l@1.0.0/fonts.css"
         rel="stylesheet"
       />
+      <ReactLenis Root />
       <Navbar />
       <Header />
       <Btn />
